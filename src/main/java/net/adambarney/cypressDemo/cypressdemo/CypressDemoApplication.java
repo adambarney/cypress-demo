04b32a62229f2a714e32bfaa4ed53f28e590a110
Adam Barney <adambarney2@gmail.com>
2018-11-04 18:59:47 -0500

    Initial commit of Initializr project

---
+++ src/main/java/net/adambarney/cypressDemo/cypressdemo/CypressDemoApplication.java
@@ -0,0 +1,12 @@
+package net.adambarney.cypressDemo.cypressdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypressDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CypressDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/net/adambarney/cypressDemo/cypressdemo/CypressDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package net.adambarney.cypressDemo.cypressdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CypressDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
